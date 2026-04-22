c7d38f015ecf848b3ad81875c02afc720ea22df1
sebasholgui-tech <sebasholgui@gmail.com>
2026-04-22 20:37:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,18 @@
-const CACHE = 'tmc-v1';
-const ASSETS = ['/'];
+const CACHE = 'tmc-v4';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => new Response('Sin conexión')));
 });
